9604f1a611145fdf269dc2ac6871cd56c9fb4c3d
Nayden Naydenov <nayden.naydenov@sap.com>
2024-09-11 12:54:07 +0300

    chore: update

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <template>
 	<div class="app">
 		<header class="app-header">
-			<ui5-shellbar primary-title="UI5 Web Components Vue Sample Application" show-notifications
-				notifications-count="2" @profile-click="handleProfileClick">
-				<img class="app-header-logo" :src=logo slot="logo" />
-				<ui5-shellbar-item icon="palette" text="Theme" ref="themeChangeItem"
-					@click="handleThemeSettingsToggle"></ui5-shellbar-item>
+			<ui5-shellbar primary-title="UI5 Web Components Vue Sample Application"
+				show-notifications
+				notifications-count="2"
+				@profile-click="handleProfileClick">
+				<img class="app-header-logo" :src=logo slot="logo"/>
+				<ui5-shellbar-item icon="palette" text="Theme" ref="themeChangeItem" @click="handleThemeSettingsToggle"></ui5-shellbar-item>
 				<ui5-avatar slot="profile" size="XS" initials="JD"></ui5-avatar>
 			</ui5-shellbar>
 		</header>
@@ -16,23 +17,22 @@
 
 		<section class="app-content">
 			<div class="create-todo-wrapper">
-				<ui5-input placeholder="Type a task..." ref="todoInput" class="add-todo-element-width"
-					id="add-input"></ui5-input>
-				<ui5-date-picker format-pattern="dd/MM/yyyy" class="add-todo-element-width" ref="todoDeadline"
-					id="date-picker"></ui5-date-picker>
-				<ui5-button class="add-todo-element-width" ref="addButton" design="Emphasized" @click="handleAdd">Add
-					Todo</ui5-button>
+				<ui5-input placeholder="Type a task..." ref="todoInput" class="add-todo-element-width" id="add-input"></ui5-input>
+				<ui5-date-picker format-pattern="dd/MM/yyyy" class="add-todo-element-width" ref="todoDeadline" id="date-picker"></ui5-date-picker>
+				<ui5-button class="add-todo-element-width" ref="addButton" design="Emphasized" @click="handleAdd">Add Todo</ui5-button>
 			</div>
 
 			<div class="list-todos-wrapper">
 				<ui5-panel class="list-todos-panel" header-text="Incompleted Tasks" :collapsed="panelCollapsed()">
-					<TodoList :todos="todos" @selection-change="handleDone" @item-deleted="handleRemove"
+					<TodoList :todos="todos" @selection-change="handleDone"
+						@item-deleted="handleRemove"
 						@item-edit="handleEdit">
 					</TodoList>
 				</ui5-panel>
 
-				<ui5-panel class="list-todos-panel" header-text="Completed Tasks" :collapsed="donePanelCollapsed()">
-					<TodoList :todos="doneTodos" @selection-change="handleUndone" @item-deleted="handleRemove"
+				<ui5-panel  class="list-todos-panel" header-text="Completed Tasks" :collapsed="donePanelCollapsed()">
+					<TodoList :todos="doneTodos" @selection-change="handleUndone"
+						@item-deleted="handleRemove"
 						@item-edit="handleEdit">
 					</TodoList>
 				</ui5-panel>
@@ -42,27 +42,33 @@
 			<div class="dialog-content">
 				<div class="edit-wrapper">
 					<ui5-label>Title:</ui5-label>
-					<ui5-textarea class="title-textarea" show-exceeded-text maxlength="24" :value="todoBeingEdittedText"
-						ref="titleEditInput">
+					<ui5-textarea class="title-textarea"
+					show-exceeded-text
+					maxlength="24"
+					:value="todoBeingEdittedText"
+					ref="titleEditInput">
 					</ui5-textarea>
 				</div>
 
 				<div class="edit-wrapper date-edit-fields">
 					<ui5-label>Date:</ui5-label>
-					<ui5-date-picker format-pattern="dd/MM/yyyy" :value="todoBeingEdittedDate"
-						ref="dateEditInput"></ui5-date-picker>
+					<ui5-date-picker format-pattern="dd/MM/yyyy" :value="todoBeingEdittedDate" ref="dateEditInput"></ui5-date-picker>
 				</div>
 			</div>
 			<div class="dialog-footer" data-ui5-slot="footer">
-				<ui5-button class="dialog-footer-btn--cancel" design="Transparent"
-					@click="cancelEdits">Cancel</ui5-button>
+				<ui5-button class="dialog-footer-btn--cancel" design="Transparent" @click="cancelEdits">Cancel</ui5-button>
 				<ui5-button class="dialog-footer-btn--save" design="Emphasized" @click="saveEdits">Save</ui5-button>
 			</div>
 		</ui5-dialog>
 
-		<ui5-popover ref="theme-settings-popover" class="app-bar-theming-popover" placement="Bottom"
-			horizontal-align="End" header-text="Theme">
-			<ui5-list ref={themeSelect} selection-mode="Single" @selection-change="handleThemeChange">
+		<ui5-popover
+				ref="theme-settings-popover"
+				class="app-bar-theming-popover"
+				placement-type="Bottom"
+				horizontal-align="Right"
+				header-text="Theme"
+		>
+			<ui5-list ref={themeSelect} mode="SingleSelect" @selection-change="handleThemeChange">
 				<ui5-li icon="palette" data-theme="sap_horizon" selected>SAP Horizon Morning</ui5-li>
 				<ui5-li icon="palette" data-theme="sap_horizon_dark">SAP Horizon Evening</ui5-li>
 				<ui5-li icon="palette" data-theme="sap_horizon_hcb">SAP Horizon HCB</ui5-li>
@@ -74,84 +80,90 @@
 			</ui5-list>
 		</ui5-popover>
 
-		<ui5-popover ref="profile-popover" id="profile-pop" class="app-bar-profile-popover" placement="Bottom"
-			horizontal-align="End">
-			<div class="profile-settings">
-				<ui5-avatar size="M" initials="JD"></ui5-avatar>
-				<div class="profile-text">
-					<ui5-title level="H3">John Doe</ui5-title>
-					<ui5-label>Vue Developer</ui5-label>
-				</div>
+		<ui5-popover 
+				ref="profile-popover"
+				id="profile-pop"
+				class="app-bar-profile-popover"
+				placement-type="Bottom"
+				horizontal-align="Right"
+		>
+		<div class="profile-settings">
+			<ui5-avatar size="M" initials="JD"></ui5-avatar>
+			<div class="profile-text">
+				<ui5-title level="H3">John Doe</ui5-title>
+				<ui5-label>Vue Developer</ui5-label>
 			</div>
+		</div>
 
-			<div class="profile-settings-list">
-				<ui5-list selection-mode="Single" separators="None" @item-click="handleProfileSettingsSelect"
-					ref="settings-popover">
-					<ui5-li icon="settings" data-key="settings">Settings</ui5-li>
-					<ui5-li icon="sys-help" data-key="help">Help</ui5-li>
-					<ui5-li icon="log" data-key="sign-out">Sign out</ui5-li>
-				</ui5-list>
-			</div>
-		</ui5-popover>
+		<div class="profile-settings-list">
+			<ui5-list mode="SingleSelect" separators="None" @item-click="handleProfileSettingsSelect" ref="settings-popover">
+				<ui5-li icon="settings" data-key="settings">Settings</ui5-li>
+				<ui5-li icon="sys-help" data-key="help">Help</ui5-li>
+				<ui5-li icon="log" data-key="sign-out">Sign out</ui5-li>
+			</ui5-list>
+		</div>
+	</ui5-popover>
 
-		<ui5-dialog id="settings-dialog" ref="settings-dialog" header-text="Profile Settings" :draggable="true">
-			<div>
-				<div class="profile-rtl-switch centered">
-					<div class="profile-rtl-switch-title">
-						<ui5-label class="profile-rtl-switch-text">RTL</ui5-label>
-					</div>
-					<ui5-switch @change="handleRtlSwitchChange"></ui5-switch>
-				</div>
-			</div>
-
+	<ui5-dialog id="settings-dialog" header-text="Profile Settings" :draggable="true">
+		<div>
 			<div class="profile-rtl-switch centered">
 				<div class="profile-rtl-switch-title">
-					<ui5-label class="profile-rtl-switch-text">Compact</ui5-label>
+					<ui5-label class="profile-rtl-switch-text">RTL</ui5-label>
 				</div>
-				<ui5-switch @change="handleContentDensitySwitchChange"></ui5-switch>
+				<ui5-switch @change="handleRtlSwitchChange"></ui5-switch>
 			</div>
+		</div>
 
-			<div class="dialog-button">
-				<ui5-button @click="handleSettingsDialogCloseButtonClick" design="Emphasized">Close</ui5-button>
+		<div class="profile-rtl-switch centered">
+			<div class="profile-rtl-switch-title">
+				<ui5-label class="profile-rtl-switch-text">Compact</ui5-label>
 			</div>
-		</ui5-dialog>
+			<ui5-switch @change="handleContentDensitySwitchChange"></ui5-switch>
+		</div>
 
-		<ui5-dialog id="help-dialog" ref="help-dialog">
-			<div slot="header" class="help-header" id="header-title-align">
-				<ui5-icon name="sys-help"></ui5-icon>
+		<div class="dialog-button">
+			<ui5-button @click="handleSettingsDialogCloseButtonClick" design="Emphasized" >Close</ui5-button>
+		</div>
+	</ui5-dialog>
+
+		<ui5-dialog id="help-dialog">
+
+		<div slot="header" class="help-header" id="header-title-align">
+			<ui5-icon name="sys-help"></ui5-icon>
 				Help
-			</div>
+		</div>
 
-			<div class="help-header" id="header-logo-align">
-				<img class="app-header-logo" alt="logo" slot="logo" src="./assets/logo.png" />
-				<ui5-title level="H5">UI5 Web Components Vue Sample App</ui5-title>
-			</div>
+		<div class="help-header" id="header-logo-align">
+			<img class="app-header-logo" alt="logo" slot="logo" src="./assets/logo.png" />
+			<ui5-title level="H5">UI5 Web Components Vue Sample App</ui5-title>
+		</div>
+			
 
-			<p class="help-dialog-text">
-				<b>Release</b>: b225.20220729335 <br>
-				<b>Server</b>: pk21443x3132 <br>
-				<b>Timestamp</b>: 2022-08-18T10:29:03.159+0200 <br>
-				<b>Company ID</b>: SAP <br>
-				<b>UI version</b>: SAP Fiori <br>
-				<b>Edition</b>: Enterprise <br>
-				<b>Admin version</b>: Vue Admin <br>
-				<hr />
-				<span class="help-dialog-text">For more information, please visit our <a
-						href="https://github.com/SAP-samples/ui5-webcomponents-sample-vue"
-						target="_blank">documentation</a>.</span>
-			</p>
+		<p class="help-dialog-text">
+			<b>Release</b>: b225.20220729335 <br>
+			<b>Server</b>: pk21443x3132 <br>
+			<b>Timestamp</b>: 2022-08-18T10:29:03.159+0200 <br>
+			<b>Company ID</b>: SAP <br>
+			<b>UI version</b>: SAP Fiori <br>
+			<b>Edition</b>: Enterprise <br>
+			<b>Admin version</b>: Vue Admin <br>
+			<hr />
+			<span class="help-dialog-text">For more information, please visit our <a href="https://github.com/SAP-samples/ui5-webcomponents-sample-vue" target="_blank">documentation</a>.</span>
+		</p>
 
-			<div class="dialog-button">
-				<ui5-button design="Emphasized" @click="handleHelpDialogCloseButtonClick">Close</ui5-button>
-			</div>
+		<div class="dialog-button">
+			<ui5-button design="Emphasized" @click="handleHelpDialogCloseButtonClick">Close</ui5-button>
+		</div>
 
-		</ui5-dialog>
+	</ui5-dialog>
 	</div>
 </template>
 
 <script>
 import logo from './assets/logo.png';
 import applyDirection from "@ui5/webcomponents-base/dist/locale/applyDirection.js";
+import '@webcomponents/custom-elements/custom-elements.min.js'
+import '@webcomponents/shadydom/shadydom.min.js'
 import { setTheme } from "@ui5/webcomponents-base/dist/config/Theme";
 import '@ui5/webcomponents-base/dist/features/F6Navigation';
 import '@ui5/webcomponents/dist/Title';
@@ -226,44 +238,42 @@ export default {
 		};
 	},
 	methods: {
-		handleThemeSettingsToggle: function (event) {
-			this.$refs["theme-settings-popover"].opener = event.detail.targetRef;
-			this.$refs["theme-settings-popover"].open = true;
+		handleThemeSettingsToggle: function(event) {
+			this.$refs["theme-settings-popover"].showAt(event.detail.targetRef);
 		},
-		handleThemeChange: function (event) {
+		handleThemeChange: function(event) {
 			setTheme(event.detail.selectedItems[0].getAttribute("data-theme"));
-			this.$refs["theme-settings-popover"].open = false;
+			this.$refs["theme-settings-popover"].close();
 		},
-		handleProfileClick: function (event) {
-			this.$refs["profile-popover"].opener = event.detail.targetRef;
-			this.$refs["profile-popover"].open = true;
+		handleProfileClick: function(event) {
+			this.$refs["profile-popover"].showAt(event.detail.targetRef);
 		},
-		handleProfileSettingsSelect: function (event) {
+		handleProfileSettingsSelect: function(event) {
 			const selectedKey = event.detail.item.getAttribute('data-key');
 			if (selectedKey === 'settings') {
-				this.$refs['settings-dialog'].open = true;
-			} else if (selectedKey === 'help') {
-				this.$refs['help-dialog'].open = true;
+				window['settings-dialog'].show(event.detail.targetRef);
+		 	} else if (selectedKey === 'help') {
+				window['help-dialog'].show(event.detail.targetRef);
 			}
 		},
-		handleRtlSwitchChange: function (event) {
+		handleRtlSwitchChange: function(event) {
 			document.body.dir = event.target.checked ? "rtl" : "ltr";
 			applyDirection();
 		},
-		handleContentDensitySwitchChange: function (event) {
+		handleContentDensitySwitchChange: function(event) {
 			if (event.target.checked) {
 				document.body.classList.add('ui5-content-density-compact');
 			} else {
 				document.body.classList.remove('ui5-content-density-compact');
 			}
 		},
-		handleSettingsDialogCloseButtonClick: function () {
-			this.$refs['settings-dialog'].open = false;
+		handleSettingsDialogCloseButtonClick: function() {
+			window['settings-dialog'].close();
 		},
-		handleHelpDialogCloseButtonClick: function () {
-			this.$refs['help-dialog'].open = false;
+		handleHelpDialogCloseButtonClick: function() {
+			window['help-dialog'].close();
 		},
-		handleAdd: function () {
+		handleAdd: function() {
 			this.todos = [...this.todos, {
 				text: this.$refs["todoInput"].value,
 				id: (this.todos.length + 1).toString(),
@@ -313,44 +323,44 @@ export default {
 			const matchedTodos = this.todos.filter(todo => todo.id.toString() === item.id);
 			let todoObj;
 
-			if (matchedTodos.length) {
-				todoObj = matchedTodos[0];
+			if (matchedTodos.length) { 
+			todoObj = matchedTodos[0];
 			} else {
-				todoObj = this.doneTodos.filter(todo => todo.id.toString() === item.id)[0];
+			todoObj = this.doneTodos.filter(todo => todo.id.toString() === item.id)[0];
 			}
 
 			this.todoBeingEdittedText = todoObj.text;
 			this.todoBeingEdittedDate = todoObj.deadline;
 			this.selectedEditTodo = todoObj.id;
 
-			this.$refs["editDialog"].open = true;
+			this.$refs["editDialog"].show();
 		},
 		saveEdits() {
 			const edittedText = this.$refs["titleEditInput"].value;
 			const edittedDate = this.$refs["dateEditInput"].value;
 
 			this.todos = this.todos.map((todo) => {
-				if (todo.id === this.selectedEditTodo) {
-					todo.text = edittedText;
-					todo.deadline = edittedDate;
-				}
+			if (todo.id === this.selectedEditTodo) {
+				todo.text = edittedText;
+				todo.deadline = edittedDate;
+			}
 
-				return todo;
+			return todo;
 			});
 
 			this.doneTodos = this.doneTodos.map((todo) => {
-				if (todo.id === this.selectedEditTodo) {
-					todo.text = edittedText;
-					todo.deadline = edittedDate;
-				}
+			if (todo.id === this.selectedEditTodo) {
+				todo.text = edittedText;
+				todo.deadline = edittedDate;
+			}
 
-				return todo;
+			return todo;
 			});
 
-			this.$refs["editDialog"].open = false;
+			this.$refs["editDialog"].close();
 		},
 		cancelEdits() {
-			this.$refs["editDialog"].open = false;
+			this.$refs["editDialog"].close();
 		},
 		panelCollapsed() {
 			return !this.todos.length ? true : undefined;
@@ -366,14 +376,13 @@ setTheme("sap_horizon");
 </script>
 
 <style>
-html,
-body {
+html, body {
 	padding: 0;
 	margin: 0;
 	height: 100%;
 	font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
-		"Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
-		sans-serif;
+	"Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
+	sans-serif;
 	-webkit-font-smoothing: antialiased;
 	-moz-osx-font-smoothing: grayscale;
 }
@@ -384,7 +393,7 @@ body {
 }
 
 .header-toolbar {
-	position: sticky;
+	position:sticky;
 	z-index: 42;
 	background-color: rgba(255, 255, 255, 0.6);
 	box-shadow: 0 4px 5px -5px #0a6ed1;
@@ -492,11 +501,11 @@ body {
 		flex-direction: column;
 	}
 
-	.add-todo-element-width {
+	 .add-todo-element-width  {
 		width: 100%;
 	}
 
-	#date-picker {
+	 #date-picker {
 		margin: 0.5rem 0 0.5rem 0;
 	}
 }
@@ -516,8 +525,7 @@ body {
 	margin-bottom: -0.425rem;
 }
 
-.profile-settings,
-.help-header {
+.profile-settings, .help-header {
 	display: flex;
 	flex-direction: row;
 	justify-content: flex-start;
@@ -551,10 +559,10 @@ body {
 	width: 100%;
 	display: flex;
 	align-items: center;
-	justify-content: space-between;
+    justify-content: space-between;
 }
 
-#header-title-align {
+#header-title-align{
 	margin: 1rem 0;
 	gap: 0.225rem;
 }
