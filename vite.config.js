dcc715edd053c32f673ca757fb4fae3b8fabd914
ilhan007 <ilhan.myumyun@sap.com>
2024-12-16 13:54:36 +0200

    chore: update to rc.1

--- vite.config.js
+++ vite.config.js
@@ -10,13 +10,6 @@ export default defineConfig({
         }
       }
   })],
-  resolve: {
-    alias: {
-      // Ensure Vite knows to resolve `thirdparty/preact` correctly.
-      '../thirdparty/preact/preact.module.js': '/node_modules/@ui5/webcomponents-base/dist/thirdparty/preact/preact.module.js',
-      '../thirdparty/preact/jsxRuntime.module.js': '/node_modules/@ui5/webcomponents-base/dist/thirdparty/preact/jsxRuntime.module.js',
-    },
-  },
   base: process.env.NODE_ENV === 'production' ? '/ui5-webcomponents-sample-vue' : '/',
   build: {
     rollupOptions: {
